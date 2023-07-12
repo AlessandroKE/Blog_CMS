e00f42a4f8f185591962a4a8a4b98e2680fafa9a
SandroKE <koomealessandro@gmail.com>
2023-07-12 14:53:55 +0300

    updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,40 +1,4 @@
-
-
-            <form method="POST" action="register.php">
-              <!-- Email input -->
-              <div class="form-outline mb-4">
-                <input type="email" name="email" id="form2Example1" class="form-control" placeholder="Email" />
-               
-              </div>
-
-              <div class="form-outline mb-4">
-                <input type="" name="username" id="form2Example1" class="form-control" placeholder="Username" />
-               
-              </div>
-
-              <!-- Password input -->
-              <div class="form-outline mb-4">
-                <input type="password" name="password" id="form2Example2" placeholder="Password" class="form-control" />
-                
-              </div>
-
-
-
-              <!-- Submit button -->
-              <button type="submit" name="submit" class="btn btn-primary  mb-4 text-center">Register</button>
-
-              <!-- Register buttons -->
-              <div class="text-center">
-                <p>Aleardy a member? <a href="login.php">Login</a></p>
-                
-
-               
-              </div>
-            </form>
-
-
-           
-        </div>
+</div>
     <!-- Footer-->
         <footer class="border-top">
             <div class="container px-4 px-lg-5">
@@ -66,7 +30,7 @@
                                 </a>
                             </li>
                         </ul>
-                        <div class="small text-center text-muted fst-italic">Copyright &copy; Your Website 2022</div>
+                        <div class="small text-center text-muted fst-italic">Copyright &copy; Sandro KE 2022</div>
                     </div>
                 </div>
             </div>
--- includes/header.php
+++ includes/header.php
@@ -43,8 +43,8 @@
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
-                            <h1>Clean Blog</h1>
-                            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                            <h1>Tech Blogs</h1>
+                            <span class="subheading">Blog website by Sandro KE</span>
                         </div>
                     </div>
                 </div>
