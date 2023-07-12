3b8a13b1910cd7a35e056e72b9003d921cd39444
SandroKE <koomealessandro@gmail.com>
2023-07-12 20:05:01 +0300

    updating backend code

--- auth/register.php
+++ auth/register.php
@@ -1,5 +1,66 @@
 <?php require '../includes/header.php'; ?>
 
+
+  <?php
+  require '../config/config.php';
+//database connection
+  $conn = getConn($host, $db_name, $user, $password);
+  $errors = array();
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if (empty($email)) {
+        $errors[] = "Please enter your email.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format.";
+    }
+
+    if (empty($username)) {
+        $errors[] = "Please enter your username.";
+    }
+
+    if (empty($password)) {
+        $errors[] = "Please enter your password.";
+    }
+
+    if (count($errors) > 0) {
+        // Display all error messages
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    } else {
+        // Form validation successful, proceed with registration logic
+        // ...
+    }
+}
+
+  if($conn){
+
+    //SQL Query to insert users data into the database
+
+    $sql = "INSERT INTO users (email, username, password) values (?,?,?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam("sss", $email, $username,$password);
+
+    if($stmt->execute()){
+
+      echo "Registration is successful";
+
+    }else{
+
+    }
+
+
+ }
+
+
+    ?>
+
+
             <form method="POST" action="register.php">
               <!-- Email input -->
               <div class="form-outline mb-4">
