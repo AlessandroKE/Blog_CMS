7ae1b86d6824ed7386b201eb9ff135892573f42c
SandroKE <koomealessandro@gmail.com>
2023-07-19 03:11:22 +0300

    initial commit

--- posts/post.php
+++ posts/post.php
@@ -1,52 +1,67 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>Clean Blog - Start Bootstrap Theme</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="../css/styles.css" rel="stylesheet" />
-    </head>
-    <body>
-        <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-            <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../index.html">Home</a></li>
-                       
-                      
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="../contact.html">Contact</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+<?php require '../includes/navbar.php'; ?>
+
+
+
+<?php
+    require '../config/config.php';
+    $conn = getConn($host, $db_name, $user, $password);
+
+    if(isset($_GET['post_id'])){
+        $id = $_GET['post_id']; // Getting the id of a single post
+
+        //$sql = "SELECT * FROM posts Where id = '$id'";
+
+        $sql = "SELECT * FROM posts WHERE id = :id"; // Updated SQL query using parameter binding
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $id); // Bind the ID parameter
+        $stmt->execute();
+    
+        $post = $stmt->fetch(PDO::FETCH_OBJ); // Assign the fetched post object to the $post variable
+    }else{
+       // echo "Invalid post";
+    } 
+
+    /* if(isset($_GET['post_id'])) {
+        $id = $_GET['post_id']; // Getting the id of a single post
+    
+        // Check if the $post variable is defined
+        if (isset($post)) {
+            echo "Post variable is defined";
+        } else {
+            // The $post variable is not defined
+            $sql = "SELECT * FROM posts WHERE id = :id"; // Updated SQL query using parameter binding
+    
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id', $id); // Bind the ID parameter
+            $stmt->execute();
+    
+            $post = $stmt->fetch(PDO::FETCH_OBJ); // Assign the fetched post object to the $post variable
+        }
+    } else {
+        // The user did not pass a post_id parameter in the URL
+        // Redirect the user to the homepage
+        //header('Location: /');
+        exit;
+    }
+     */
+?>
+        
         <!-- Page Header-->
         <header class="masthead" style="background-image: url('../assets/img/post-bg.jpg')">
             <div class="container position-relative px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="post-heading">
-                            <h1>Man must explore, and this is exploration at its greatest</h1>
-                            <h2 class="subheading">Problems look mighty small from 150 miles up</h2>
-                            <span class="meta">
-                                Posted by
-                                <a href="#!">Start Bootstrap</a>
-                                on August 24, 2022
-                            </span>
+                            <?php //if(isset($post)): ?> <!-- Add a check if $post is set -->
+                                <h1><?php echo $post->title; ?></h1>
+                                <h2 class="subheading"><?php echo $post->subtitle; ?></h2>
+                                <span class="meta">
+                                    Posted by
+                                    <a href="#!"><?php echo $post->username ?></a>
+                                    <?php echo date('M', strtotime($post->created_at)) . ', ' . date('d', strtotime($post->created_at)) . ', ' . date('Y', strtotime($post->created_at)); ?>
+                                </span>
+                            <?php //endif; ?>
                         </div>
                     </div>
                 </div>
@@ -83,45 +98,4 @@
                 </div>
             </div>
         </article>
-        <!-- Footer-->
-        <footer class="border-top">
-            <div class="container px-4 px-lg-5">
-                <div class="row gx-4 gx-lg-5 justify-content-center">
-                    <div class="col-md-10 col-lg-8 col-xl-7">
-                        <ul class="list-inline text-center">
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                            <li class="list-inline-item">
-                                <a href="#!">
-                                    <span class="fa-stack fa-lg">
-                                        <i class="fas fa-circle fa-stack-2x"></i>
-                                        <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                                    </span>
-                                </a>
-                            </li>
-                        </ul>
-                        <div class="small text-center text-muted fst-italic">Copyright &copy; Your Website 2022</div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <!-- Bootstrap core JS-->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="../js/scripts.js"></script>
-    </body>
-</html>
+<?php require '../includes/footer.php'; ?>
