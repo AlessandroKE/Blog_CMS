4eef3305413c26d9fa54ce7e8bbf948186eadbd4
SandroKE <koomealessandro@gmail.com>
2023-07-22 12:47:34 +0300

    updating favicon icon

--- includes/header.php
+++ includes/header.php
@@ -15,8 +15,8 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Tech Blogs - By Sandro KE</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <title>Blogs</title>
+        <link rel="shortcut icon" type="image/x-icon" href="assets/apple-touch-icon.png" />
         <!-- Font Awesome icons (free version)-->
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
