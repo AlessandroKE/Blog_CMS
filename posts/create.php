b98b37a9865cc505c9763350918d4922255ff013
SandroKE <koomealessandro@gmail.com>
2023-07-24 23:32:23 +0300

    updated

--- posts/create.php
+++ posts/create.php
@@ -89,6 +89,15 @@
                 <textarea type="text" name="body" id="form2Example1" class="form-control" placeholder="body" rows="8"></textarea>
             </div>
 
+            <div class="form-outline mb-4">
+             <select class="form-select" aria-label="Default select example">
+                <option selected>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+             </div>
+
               
              <div class="form-outline mb-4">
                 <input type="file" name="img" id="form2Example1" class="form-control" placeholder="image" />
