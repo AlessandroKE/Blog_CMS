0c1b3ac11909e1daf5f2e48e09732695906534ae
SandroKE <koomealessandro@gmail.com>
2023-08-02 15:01:06 +0300

    Displaying data on the dashboard from the database

--- admin-panel/index.php
+++ admin-panel/index.php
@@ -7,6 +7,28 @@
     // echo "Logged in sucessfully";
     header("Location: http://localhost/Blog_CMS/admin-panel/admins/login-admins.php");
   }
+  $conn = getConn($host, $db_name, $user, $password);
+
+    //Number of admins
+    $sql = "SELECT COUNT(*) AS admins_number FROM  admins";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $admins = $stmt->fetch(PDO::FETCH_OBJ);
+
+    // Number of categories
+    $sql = "SELECT COUNT(*) AS categories_number FROM  categories";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $categories = $stmt->fetch(PDO::FETCH_OBJ);
+
+    // Number of posts
+    $sql = "SELECT COUNT(*) AS posts_number FROM  posts";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+
+    $posts = $stmt->fetch(PDO::FETCH_OBJ);
 
     ?>  
       <div class="row">
@@ -15,7 +37,7 @@
             <div class="card-body">
               <h5 class="card-title">Posts</h5>
               <!-- <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6> -->
-              <p class="card-text">number of posts: 8</p>
+              <p class="card-text">number of posts: <?php echo $posts->posts_number ?></p>
              
             </div>
           </div>
@@ -25,7 +47,7 @@
             <div class="card-body">
               <h5 class="card-title">Categories</h5>
               
-              <p class="card-text">number of categories: 4</p>
+              <p class="card-text">number of categories: <?php echo $categories->categories_number ?></p>
               
             </div>
           </div>
@@ -35,45 +57,11 @@
             <div class="card-body">
               <h5 class="card-title">Admins</h5>
               
-              <p class="card-text">number of admins: 3</p>
+              <p class="card-text">number of admins: <?php echo $admins->admins_number; ?></p>
               
             </div>
           </div>
         </div>
       </div>
-     <!--  <div class="row">
-        <div class="col">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table> -->
+
 <?php require 'layout/footer.php'; ?>     
