33c0d4ed5af573b4de08aedc89d2c85286b26f63
SandroKE <koomealessandro@gmail.com>
2023-08-23 03:19:54 +0300

    initial commit

---
+++ admin-panel/posts-admins/delete-posts.php
@@ -0,0 +1,25 @@
+<?php
+
+ require '../../config/config.php';
+ $conn = getConn($host, $db_name, $user, $password);
+
+if(isset($_GET['del_id'])){
+    $id = $_GET['del_id'];
+
+    $sql = "DELETE FROM posts WHERE id = :id";
+    $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $id); // Bind the ID parameter
+        
+        
+        if($stmt->execute()){
+
+        header("location: http://localhost/Blog_CMS/admin-panel/posts-admins/show-posts.php");
+        }
+
+}else {
+    header("Location:http://localhost/Blog_CMS/404.php");
+}
+
+
+
+?>
